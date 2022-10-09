ae1fe1f4bdf1a8d4858679e079812de7ad7584c6
Mlie <emipa606@users.noreply.github.com>
2022-10-09 20:57:23 +0200

    Mod updated for 1.4 and passed autotests. Used DeepL to update translations for ChineseSimplified, Czech, Danish, Dutch, Estonian, Finnish, French, German, Hungarian, Italian, Japanese, Polish, Portuguese, Romanian, Russian, Slovak, Spanish, Swedish, Turkish

--- Source/Insulation/Controller.cs
+++ Source/Insulation/Controller.cs
@@ -1,3 +1,4 @@
+using Mlie;
 using UnityEngine;
 using Verse;
 
@@ -6,10 +7,13 @@ namespace Insulation;
 public class Controller : Mod
 {
     public static Settings Settings;
+    public static string currentVersion;
 
     public Controller(ModContentPack content) : base(content)
     {
         Settings = GetSettings<Settings>();
+        currentVersion =
+            VersionFromManifest.GetVersionFromModMetaData(ModLister.GetActiveModWithIdentifier("Mlie.Insulation"));
     }
 
     public override string SettingsCategory()
--- Source/Insulation/Settings.cs
+++ Source/Insulation/Settings.cs
@@ -9,13 +9,23 @@ public class Settings : ModSettings
 
     public void DoWindowContents(Rect canvas)
     {
-        var listing_Standard = new Listing_Standard();
-        listing_Standard.ColumnWidth = canvas.width;
+        var listing_Standard = new Listing_Standard
+        {
+            ColumnWidth = canvas.width
+        };
         listing_Standard.Begin(canvas);
         listing_Standard.Gap();
         listing_Standard.Label("Insulation.pctEffective".Translate() + "  " + pctEffective);
         pctEffective = checked((int)listing_Standard.Slider(pctEffective, 50f, 200f));
         listing_Standard.Gap();
+        if (Controller.currentVersion != null)
+        {
+            listing_Standard.Gap();
+            GUI.contentColor = Color.gray;
+            listing_Standard.Label("Insulation.Modversion".Translate(Controller.currentVersion));
+            GUI.contentColor = Color.white;
+        }
+
         listing_Standard.End();
     }
 
